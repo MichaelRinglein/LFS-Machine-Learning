9ce2d7294d544850c12ac735944a2fd8ea322c59
MichaelRinglein <michaelringlein@gmail.com>
2018-06-05 23:34:07 +0300

    live-for-speed-ai.py for checking Houghes Lines

--- live-for-speed-ai.py
+++ live-for-speed-ai.py
@@ -138,26 +138,6 @@ def process_img(image):
 
     return processed_img, original_image, m1, m2
 
-def straight():
-    PressKey(W)
-    ReleaseKey(A)
-    ReleaseKey(D)
-
-def left():
-    PressKey(A)
-    ReleaseKey(W)
-    ReleaseKey(D)
-
-def right():
-    PressKey(D)
-    ReleaseKey(A)
-    ReleaseKey(W)
-
-def slow_ya_roll():
-    ReleaseKey(W)
-    ReleaseKey(A)
-    ReleaseKey(D)
-
 for i in list(range(4))[::-1]:
     print(i+1)
     time.sleep(1)
@@ -171,15 +151,8 @@ def main():
         last_time = time.time()
         new_screen, original_image, m1, m2 = process_img(screen)
         cv2.imshow('window', new_screen)
-        cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-        cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-
-        if m1 < 0 and m2 < 0:
-            right()
-        elif m1 > 0  and m2 > 0:
-            left()
-        else:
-            straight()
+        #cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
